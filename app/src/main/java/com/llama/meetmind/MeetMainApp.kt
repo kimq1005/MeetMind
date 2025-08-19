57aee685e5f5c808c372f06363b4c0325bd8f211
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2025-08-19 15:58:57 +0900

    [feat] 네이버 의존성 추가 - 네이버 맵 띄우기

--- app/src/main/java/com/llama/meetmind/MeetMainApp.kt
+++ app/src/main/java/com/llama/meetmind/MeetMainApp.kt
@@ -1,4 +1,13 @@
 package com.llama.meetmind
 
 import android.app.Application
+import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MeetMainApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NcpKeyClient("kjk7ogkoul")
+    }
+}
--- presentation/src/main/java/com/llama/presentation/SplashActivity.kt
+++ presentation/src/main/java/com/llama/presentation/SplashActivity.kt
@@ -1,26 +1,33 @@
 package com.llama.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.compose.CameraPositionState
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
 
 @OptIn(ExperimentalNaverMapApi::class)
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Log.d("TAG", "naver: 아니 아")
+            NaverMapSdk.OnAuthFailedListener {
+                val wow = it.errorCode
+                Log.d("TAG", "naver 인증 실패: $wow")
+            }
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -28,12 +35,16 @@ class SplashActivity : AppCompatActivity() {
                         color = Color.White
                     ),
             ) {
+                val seoul = LatLng(37.532600, 127.024612)
+                val cameraPositionState: CameraPositionState = rememberCameraPositionState {
+                    // 카메라 초기 위치를 설정합니다.
+                    position = CameraPosition(seoul, 11.0)
+                }
+
                 NaverMap(
                     modifier = Modifier
                         .fillMaxSize(),
-                    cameraPositionState = CameraPositionState(
-                        position =  com.naver.maps.map.NaverMap.DEFAULT_CAMERA_POSITION,
-                    )
+                    cameraPositionState = cameraPositionState,
                 )
             }
         }
